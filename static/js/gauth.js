c981989312dec56f5e7b6adfe20482de786ac44d
pakro <893843891@qq.com>
2022-08-14 17:50:51 +0800

    格式化代码

--- static/js/gauth.js
+++ static/js/gauth.js
@@ -17,18 +17,18 @@
 (function (exports) {
     "use strict";
 
-    var StorageService = function () {
-        var setObject = function (key, value) {
+    let StorageService = function () {
+        let setObject = function (key, value) {
             localStorage.setItem(key, JSON.stringify(value));
         };
 
-        var getObject = function (key) {
-            var value = localStorage.getItem(key);
+        let getObject = function (key) {
+            let value = localStorage.getItem(key);
             // if(value) return parsed JSON else undefined
             return value && JSON.parse(value);
         };
 
-        var isSupported = function () {
+        let isSupported = function () {
             return typeof (Storage) !== "undefined";
         };
 
@@ -45,43 +45,43 @@
     // Originally based on the JavaScript implementation as provided by Russell Sayers on his Tin Isles blog:
     // http://blog.tinisles.com/2011/10/google-authenticator-one-time-password-algorithm-in-javascript/
 
-    var KeyUtilities = function (jsSHA) {
+    let KeyUtilities = function (jsSHA) {
 
-        var dec2hex = function (s) {
+        let dec2hex = function (s) {
             return (s < 15.5 ? '0' : '') + Math.round(s).toString(16);
         };
 
-        var hex2dec = function (s) {
+        let hex2dec = function (s) {
             return parseInt(s, 16);
         };
 
-        var base32tohex = function (base32) {
-            var base32chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-            var bits = "";
-            var hex = "";
+        let base32tohex = function (base32) {
+            let base32chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+            let bits = "";
+            let hex = "";
 
-            for (var i = 0; i < base32.length; i++) {
-                var val = base32chars.indexOf(base32.charAt(i).toUpperCase());
+            for (let i = 0; i < base32.length; i++) {
+                let val = base32chars.indexOf(base32.charAt(i).toUpperCase());
                 bits += leftpad(val.toString(2), 5, '0');
             }
 
-            for (i = 0; i + 4 <= bits.length; i += 4) {
-                var chunk = bits.substr(i, 4);
+            for (let i = 0; i + 4 <= bits.length; i += 4) {
+                let chunk = bits.substring(i, 4);
                 hex = hex + parseInt(chunk, 2).toString(16);
             }
 
             return hex;
         };
 
-        var leftpad = function (str, len, pad) {
+        let leftpad = function (str, len, pad) {
             if (len + 1 >= str.length) {
                 str = new Array(len + 1 - str.length).join(pad) + str;
             }
             return str;
         };
 
-        var generate = function (secret, epoch) {
-            var key = base32tohex(secret);
+        let generate = function (secret, epoch) {
+            let key = base32tohex(secret);
 
             // HMAC generator requires secret key to have even number of nibbles
             if (key.length % 2 !== 0) {
@@ -92,19 +92,19 @@
             if (typeof epoch === 'undefined') {
                 epoch = Math.round(new Date().getTime() / 1000.0);
             }
-            var time = leftpad(dec2hex(Math.floor(epoch / 30)), 16, '0');
+            let time = leftpad(dec2hex(Math.floor(epoch / 30)), 16, '0');
 
             // external library for SHA functionality
-            var hmacObj = new jsSHA(time, "HEX");
-            var hmac = hmacObj.getHMAC(key, "HEX", "SHA-1", "HEX");
+            let hmacObj = new jsSHA(time, "HEX");
+            let hmac = hmacObj.getHMAC(key, "HEX", "SHA-1", "HEX");
 
-            var offset = 0;
+            let offset = 0;
             if (hmac !== 'KEY MUST BE IN BYTE INCREMENTS') {
                 offset = hex2dec(hmac.substring(hmac.length - 1));
             }
 
-            var otp = (hex2dec(hmac.substr(offset * 2, 8)) & hex2dec('7fffffff')) + '';
-            return (otp).substr(otp.length - 6, 6).toString();
+            let otp = (hex2dec(hmac.substring(offset * 2, 8)) & hex2dec('7fffffff')) + '';
+            return (otp).substring(otp.length - 6, 6).toString();
         };
 
         // exposed functions
@@ -116,12 +116,87 @@
     exports.KeyUtilities = KeyUtilities;
 
     // ----------------------------------------------------------------------------
-    var KeysController = function () {
-        var storageService = null,
+    exports.KeysController = function () {
+        let timerTick = function () {
+            let epoch = Math.round(new Date().getTime() / 1000.0);
+            let countDown = 30 - (epoch % 30);
+            if (epoch % 30 === 0) {
+                updateKeys();
+            }
+            $('#updatingIn').text(countDown);
+        };
+        let deleteAccount = function (index) {
+            // Remove object by index
+            let accounts = storageService.getObject('accounts');
+            accounts.splice(index, 1);
+            storageService.setObject('accounts', accounts);
+
+            updateKeys();
+        };
+        let toggleEdit = function () {
+            editingEnabled = !editingEnabled;
+            if (editingEnabled) {
+                $('#addButton').show();
+            } else {
+                $('#addButton').hide();
+            }
+            updateKeys();
+        };
+        let updateKeys = function () {
+            let accountList = $('#accounts');
+            // Remove all except the first line
+            accountList.find("li:gt(0)").remove();
+
+            $.each(storageService.getObject('accounts'), function (index, account) {
+                let key = keyUtilities.generate(account.secret);
+
+                // Construct HTML
+                let detLink = $('<h3>' + key + '</h3><p>' + account.name + '</p>');
+                let accElem = $('<li data-icon="false">').append(detLink);
+
+                if (editingEnabled) {
+                    let delLink = $('<p class="ui-li-aside"><a class="ui-btn-icon-notext ui-icon-delete" href="#"></a></p>');
+                    delLink.onclick(function () {
+                        deleteAccount(index);
+                    });
+                    accElem.append(delLink);
+                }
+
+                // Add HTML element
+                accountList.append(accElem);
+            });
+            accountList.listview().listview('refresh');
+        };
+        let addAccount = function (name, secret) {
+            if (secret === '') {
+                // Bailout
+                return false;
+            }
+
+            // Construct JSON object
+            const account = {
+                'name': name,
+                'secret': secret
+            };
+
+            // Persist new object
+            let accounts = storageService.getObject('accounts');
+            if (!accounts) {
+                // if undefined create a new array
+                accounts = [];
+            }
+            accounts.push(account);
+            storageService.setObject('accounts', accounts);
+
+            updateKeys();
+
+            return true;
+        };
+        let storageService = null,
             keyUtilities = null,
             editingEnabled = false;
 
-        var init = function () {
+        let init = function () {
             storageService = new StorageService();
             keyUtilities = new KeyUtilities(jsSHA);
 
@@ -141,7 +216,7 @@
             }
 
             // Bind to keypress event for the input
-            $('#addKeyButton').click(function () {
+            $('#addKeyButton').onclick(function () {
                 var name = $('#keyAccount').val();
                 var secret = $('#keySecret').val();
                 // remove spaces from secret
@@ -155,110 +230,21 @@
                 }
             });
 
-            $('#addKeyCancel').click(function () {
+            $('#addKeyCancel').onclick(function () {
                 clearAddFields();
             });
 
-            var clearAddFields = function () {
+            let clearAddFields = function () {
                 $('#keyAccount').val('');
                 $('#keySecret').val('');
             };
 
-            $('#edit').click(function () {
+            $('#edit').onclick(function () {
                 toggleEdit();
             });
-            $('#export').click(function () {
+            $('#export').onclick(function () {
                 // exportAccounts();
             });
-        };
-
-        var updateKeys = function () {
-            var accountList = $('#accounts');
-            // Remove all except the first line
-            accountList.find("li:gt(0)").remove();
-
-            $.each(storageService.getObject('accounts'), function (index, account) {
-                var key = keyUtilities.generate(account.secret);
-
-                // Construct HTML
-                var detLink = $('<h3>' + key + '</h3><p>' + account.name + '</p>');
-                var accElem = $('<li data-icon="false">').append(detLink);
-
-                if (editingEnabled) {
-                    var delLink = $('<p class="ui-li-aside"><a class="ui-btn-icon-notext ui-icon-delete" href="#"></a></p>');
-                    delLink.click(function () {
-                        deleteAccount(index);
-                    });
-                    accElem.append(delLink);
-                }
-
-                // Add HTML element
-                accountList.append(accElem);
-            });
-            accountList.listview().listview('refresh');
-        };
-
-        var toggleEdit = function () {
-            editingEnabled = !editingEnabled;
-            if (editingEnabled) {
-                $('#addButton').show();
-            } else {
-                $('#addButton').hide();
-            }
-            updateKeys();
-        };
-
-        var exportAccounts = function () {
-            var accounts = JSON.stringify(storageService.getObject('accounts'));
-            var blob = new Blob([accounts], {
-                type: 'text/plain;charset=utf-8'
-            });
-
-            saveAs(blob, 'gauth-export.json');
-        };
-
-        var deleteAccount = function (index) {
-            // Remove object by index
-            var accounts = storageService.getObject('accounts');
-            accounts.splice(index, 1);
-            storageService.setObject('accounts', accounts);
-
-            updateKeys();
-        };
-
-        var addAccount = function (name, secret) {
-            if (secret === '') {
-                // Bailout
-                return false;
-            }
-
-            // Construct JSON object
-            var account = {
-                'name': name,
-                'secret': secret
-            };
-
-            // Persist new object
-            var accounts = storageService.getObject('accounts');
-            if (!accounts) {
-                // if undefined create a new array
-                accounts = [];
-            }
-            accounts.push(account);
-            storageService.setObject('accounts', accounts);
-
-            updateKeys();
-
-            return true;
-        };
-
-        var timerTick = function () {
-            var epoch = Math.round(new Date().getTime() / 1000.0);
-            var countDown = 30 - (epoch % 30);
-            if (epoch % 30 === 0) {
-                updateKeys();
-            }
-            $('#updatingIn').text(countDown);
         };
 
         return {
@@ -267,7 +253,5 @@
             deleteAccount: deleteAccount
         };
     };
-
-    exports.KeysController = KeysController;
 
 })(typeof exports === 'undefined' ? this['gauth'] = {} : exports);
